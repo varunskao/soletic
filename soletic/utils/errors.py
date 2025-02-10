4ac5aa513b65b50968c503edc275f8f0f9dce219
Varun Kao <varunskao@gmail.com>
2025-02-10 10:12:24 -0500

    Improve RPC client errors

--- soletic/utils/errors.py
+++ soletic/utils/errors.py
@@ -25,6 +25,15 @@ class ProgramStateNotSupported(NotImplementedError):
 
 class HeliusAPIError(Exception):
 
+    code_to_msg = {
+        401: "Unauthorized. Invalid API key or restricted access due to Access Control Rules.",
+        429: "Too Many Requests. Exceeded Rate Limits.",
+        500: "Internal Server Error. Contact Helius support for assistance",
+        503: "Service Unavailable. Server is temporarily overloaded or under maintenance.",
+        504: "Gateway Timeout",
+    }
+
     def __init__(self, message, status_code):
         super().__init__(message)
         self.status_code = status_code
+        self.error_message = self.code_to_msg.get(status_code, "Unknown Error Code")
